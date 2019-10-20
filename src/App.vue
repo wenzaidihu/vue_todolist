bd565472bc3a9fec0131ff52015a7277181f6cd6
wenzaidihu <750625920@qq.com>
2019-10-20 17:23:16 +0800

    add bootstrap jquery normalize.css to project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    
     <HelloWorld msg="恶心心的臭大宝"/>
   </div>
 </template>
@@ -18,11 +18,6 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import $ from "jquery";
 import Vue from "vue";
 import App from "./App.vue";
+import "normalize.css/normalize.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 Vue.config.productionTip = false;
 
