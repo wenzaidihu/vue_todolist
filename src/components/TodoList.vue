33b9a26f62ef77e13375bec60ac717ffdd52e2b9
wenzaidihu <750625920@qq.com>
2019-10-20 18:06:24 +0800

    更改项目文件结构

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="todo-list-wrapper">
+    todo-list-wrapper
+    
+  </div>
+</template>
+
+<script>
+import TodoListItem from "@components/TodoListItem.vue";
+export default {
+  name:"TodoList",
+  components: {
+    TodoListItem
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="todo-list-item-wrapper">
+    todo-list-item-wrapper
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoListItem"
+ 
+};
+</script>
+
+<style scoped>
+
+</style>
